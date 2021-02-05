7be8575f28c897e13c8223fb1bcde9d27d2469cc
山口紡 <tsumugu1515@gmail.com>
2021-02-05 16:16:10 +0900

    feat: 人の頭のシルエットでマスクする機能を実装

--- gen.php
+++ gen.php
@@ -1,13 +1,13 @@
 <?php
 $words = urldecode($_GET["words"]);
-$command = "sudo /usr/bin/python3.6 /var/www/html/wordcloud/simple.py '".$words."' 2>&1";
-exec($command, $errors);
-if (count($errors) > 0) {
-  // Error occured: return HTTP Status Code 400(Bad Request)
-  header('HTTP', true, 400);
+$uid_get = basename(urldecode($_GET["uid"]));
+$uid = empty($uid_get)?"?uid?":$uid_get;
+$command = "sudo /usr/bin/python3.6 /var/www/html/wordcloud/simple.py '".$words."' '".$uid."' 2>&1";
+$exec_res = exec($command, $error);
+if (strpos($exec_res, "/var/www/html/wordcloud/imgs/") === false) {
+  header("HTTP", true, 400);
   echo "400 Bad Request";
 } else {
-  // Succeed: return JPEG binary
-  header('Content-Type: image/jpg');
-  echo file_get_contents("./test.jpg");
+  header("Content-Type: image/jpg");
+  echo file_get_contents($exec_res);
 }
--- simple.py
+++ simple.py
@@ -1,18 +1,16 @@
-"""
-Thanks to wordcloud Minimal Example (https://amueller.github.io/word_cloud/auto_examples/simple.html)
-"""
-
 import os
 from os import path
 from wordcloud import WordCloud
+from PIL import Image
+from PIL import ImageFilter
+import numpy as np
 import sys
 import json
+import uuid
 
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 
-# convert JSON to Space Splited String
-#ex)
-#  {"test": 5, "fizz": 2, "buzz": 3} => "test test test test test fizz fizz buzz buzz buzz"
+# gen text
 text = ""
 try:
   json_dict = json.loads(sys.argv[1])
@@ -25,14 +23,27 @@ except:
   print('Error!')
   exit()
 
-# generate wordcloud
+# Generate a word cloud image
 font_path = path.join(d, 'NotoSansJP-Light.otf')
+mask = np.array(Image.open(path.join(d, "mask2.png")))
 wordcloud = WordCloud(background_color='white',
+                      #background_color='black',
                       font_path = font_path,
                       min_font_size=15,
                       max_font_size=200,
+                      mask=mask,
+                      contour_width=1,
+                      contour_color='gray',
+                      #contour_color='white',
                       collocations=False,
                       width=1280,
                       height=720)
+
 image = wordcloud.generate(text).to_image()
-image.save(path.join(d, 'test.jpg'), quality=100)
+if(len(sys.argv) == 3):
+  imgfile_name = str(uuid.uuid4()) if sys.argv[2] == "?uid?" else sys.argv[2]
+else:
+  imgfile_name = str(uuid.uuid4())
+image_path = '/var/www/html/wordcloud/imgs/'+imgfile_name+'.jpg'
+image.save(image_path, quality=100)
+print(image_path)
